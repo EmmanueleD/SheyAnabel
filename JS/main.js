4f2f96e1b96dcc6a8163d6bc645825b039b8a6c8
Emmanuele-D <emmanuele.durante@arkadiagroup.it>
2021-10-23 20:30:05 +0200

    first commit

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+const firstBtn = document.querySelector("#btn-First-CTA");
+const emmanuele = document.querySelector(".emmanuele");
+
+firstBtn.addEventListener("click", function(){
+  return window.open("https://form.jotform.com/212277811830049", '_blank').focus()
+})
+
+emmanuele.addEventListener("click", function(){
+  return window.open("https://www.emmanuele.xyz/", '_blank').focus()
+})
