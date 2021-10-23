d1bfec566f9316a41df469b9348df1d4681a8160
Emmanuele-D <emmanuele.durante@arkadiagroup.it>
2021-10-23 20:42:09 +0200

    add products links

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,11 @@
 const firstBtn = document.querySelector("#btn-First-CTA");
 const emmanuele = document.querySelector(".emmanuele");
+const strawberry = document.querySelector("#strawberry");
+const recover = document.querySelector("#recover");
+const beachbar = document.querySelector("#beachbar");
+const energize = document.querySelector("#energize");
+const collagen = document.querySelector("#collagen");
+
 
 firstBtn.addEventListener("click", function(){
   return window.open("https://form.jotform.com/212277811830049", '_blank').focus()
@@ -7,4 +13,24 @@ firstBtn.addEventListener("click", function(){
 
 emmanuele.addEventListener("click", function(){
   return window.open("https://www.emmanuele.xyz/", '_blank').focus()
+})
+
+strawberry.addEventListener("click", function(){
+  return window.open("https://www.teambeachbody.com/shop/us/d/strawberry-shakeology-SHKStrawberry?referringRepId=2293408", '_blank').focus()
+})
+
+recover.addEventListener("click", function(){
+  return window.open("https://www.teambeachbody.com/shop/us/d/BBPRecoverAllFlavors?referringRepId=2293408", '_blank').focus()
+})
+
+beachbar.addEventListener("click", function(){
+  return window.open("https://www.teambeachbody.com/shop/us/d/beachbar-2-box-pack-BBar2BoxCombo?referringRepId=2293408", '_blank').focus()
+})
+
+energize.addEventListener("click", function(){
+  return window.open("https://www.teambeachbody.com/shop/us/d/BBPEnergizeAllFlavors?referringRepId=2293408", '_blank').focus()
+})
+
+collagen.addEventListener("click", function(){
+  return window.open("https://www.teambeachbody.com/shop/us/d/BeachbodyCollagenBoost?referringRepId=2293408", '_blank').focus()
 })
